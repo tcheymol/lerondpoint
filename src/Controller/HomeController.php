12620cbc19e6ff2efe3d538d932c8e9a60f0c8c7
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-08-07 16:33:57 +0200

    :sparkles: [Login page] Clean and use common template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,10 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
-
         return $this->render('home/index.html.twig');
     }
 }
