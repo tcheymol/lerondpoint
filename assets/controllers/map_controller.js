8aeb27f74689071cf42dab4f23ec2d1a173b5215
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-09-23 13:37:08 +0200

    :sparkles: [Maps] Add a few example pins

--- assets/controllers/map_controller.js
+++ assets/controllers/map_controller.js
@@ -11,47 +11,26 @@ export default class extends Controller {
     }
 
     initMap() {
-        const map = L.map('map').setView([46.966409, 2.483529], 5.49);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        const map = L.map('map').setView([46.966409, 2.483529], 5);
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        // var marker = L.marker([51.5, -0.09]).addTo(map);
+        var greenIcon = L.icon({
+            iconUrl: 'rp1.png',
+            shadowUrl: 'leaf-shadow.png',
+            iconSize:     [35, 35], // size of the icon
+        });
 
-        // map.setView([46.9, 1.4519], 5);
-        // // const greenIcon = L.icon({
-        // //     iconUrl: $('#map').data('marker'),
-        // //     iconSize: [25, 40],
-        // //     iconAnchor: [12, 38],
-        // //     popupAnchor: [0, -35]
-        // // });
-        // const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     maxZoom: 19,
-        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Les contributeurs d’OpenStreetMap</a>'
-        // }).addTo(map);
-
-        // $.get('/public/get-centers', (data) => {
-        //     const centers = data[0];
-        //     getPopupTemplate(centers);
-        // });
-
-        // const getPopupTemplate = (centers) => {
-        //     $.each(centers, (key, center) => {
-        //             const {lat, lng, pays, nom, ville} = center.adresse;
-        //             if (lat !== null && lng !== null && pays.toLowerCase() === 'france') {
-        //                 let marker = L.marker([lat, lng], {icon: greenIcon}).addTo(map);
-        //                 marker.bindPopup(`<b>
-        //                     <div>
-        //                         <h6 class="text-primary">
-        //                             ${center.nom}
-        //                         </h6>
-        //                         <img style="width: 12px;" src="build/images/homeV2/reconnect-centers-home.3d2de150.png" alt="">
-        //                         <small>${nom} - ${ville}</small>
-        //                     </div>
-        //                 </b>`);
-        //             }
-        //         }
-        //     )
-        // }
+        L.marker([47, 2], {icon: greenIcon}).addTo(map);
+        L.marker([48, 4], {icon: greenIcon}).addTo(map);
+        L.marker([48, 7], {icon: greenIcon}).addTo(map);
+        L.marker([48, -4], {icon: greenIcon}).addTo(map);
+        L.marker([49, -1], {icon: greenIcon}).addTo(map);
+        L.marker([46, -1], {icon: greenIcon}).addTo(map);
+        L.marker([44, 7], {icon: greenIcon}).addTo(map);
+        L.marker([46, 2], {icon: greenIcon}).addTo(map);
+        L.marker([44,1], {icon: greenIcon}).addTo(map);
+        L.marker([46, 3], {icon: greenIcon}).addTo(map);
     }
 }
