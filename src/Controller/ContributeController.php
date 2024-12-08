a11b8c7e1cb5d7e4a028cec19f9b58eb6c4ddc67
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-12-08 18:08:39 +0100

    :recycle: [Participate] Rename to contribute

---
+++ src/Controller/ContributeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContributeController extends AbstractController
+{
+    #[Route('/contribute', name: 'app_contribute')]
+    public function index(): Response
+    {
+        return $this->render('contribute/index.html.twig', [
+            'controller_name' => 'ContributeController',
+        ]);
+    }
+}
