b40a758e674a45e346a807560c8ff36113f706a2
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-12-22 23:22:18 +0100

    :art: [Home] Improve scroll behavior

--- assets/controllers/show_hide_controller.js
+++ assets/controllers/show_hide_controller.js
@@ -4,23 +4,39 @@ import { Controller } from '@hotwired/stimulus';
 * The following line makes this controller "lazy": it won't be downloaded until needed
 * See https://github.com/symfony/stimulus-bridge#lazy-controllers
 */
+function toggleHeader(scrollDirection) {
+    Array.prototype.forEach.call(document.getElementsByClassName('navbar'),
+        function(element) {
+            if ('down' === scrollDirection && 50 < document.documentElement.scrollTop) {
+                element.style.display = 'none';
+                document.getElementById('scrollDownButton').style.display = 'none';
+            } else if ('up' === scrollDirection && 50 > document.documentElement.scrollTop) {
+                element.style.display = 'flex';
+                document.getElementById('scrollDownButton').style.display = 'block';
+            }
+        }
+    );
+}
+
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    connect() {
-        document.body.style.marginTop = '85vh';
-        window.addEventListener("scroll", () => {
-            this.show();
-        });
-        setTimeout(this.show, 5000)
-    }
+    lastScrollPosition = 0;
 
-    show = () => {
-        Array.prototype.forEach.call(document.getElementsByClassName('show-on-show-hide'),
-            function(element) {
-                element.style.top = '0';
+    connect() {
+            window.addEventListener("scroll", () => this.show());
+        }
+
+        show = () => {
+            const currentScrollPosition = document.documentElement.scrollTop;
+            const scrollDirection = currentScrollPosition >= this.lastScrollPosition ? 'down' : 'up';
+            this.lastScrollPosition = currentScrollPosition;
+            toggleHeader(scrollDirection);
+
+            if ('down' === scrollDirection && 0 === this.lastScrollPosition) {
+                window.scrollTo({
+                    top: window.innerHeight,
+                    behavior: 'smooth'
+                });
             }
-        );
-        document.body.style.marginTop = '0';
-        this.element.remove();
-    }
+        }
 }
