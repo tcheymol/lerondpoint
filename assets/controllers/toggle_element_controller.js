b03f9c62f2dc86e52519f0175323e5e040854583
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2025-04-14 10:19:00 +0200

    :sparkles: [Add Track] Improove UX

---
+++ assets/controllers/toggle_element_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from '@hotwired/stimulus';
+
+/*
+* The following line makes this controller "lazy": it won't be downloaded until needed
+* See https://github.com/symfony/stimulus-bridge#lazy-controllers
+*/
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static values = {
+        showElementId: String,
+        hideElementId: String,
+        hideSecondElementId: String,
+    }
+
+    show() {
+        if (this.hasShowElementIdValue) {
+            const showElement = document.getElementById(this.showElementIdValue);
+            if (showElement) {
+                showElement.classList.remove('hidden');
+            }
+        }
+        if (this.hasHideElementIdValue) {
+            const hideElement = document.getElementById(this.hideElementIdValue);
+            if (hideElement) {
+                hideElement.classList.add('hidden');
+            }
+        }if (this.hasHideSecondElementIdValue) {
+            const hideSecondElement = document.getElementById(this.hideSecondElementIdValue);
+            if (hideSecondElement) {
+                hideSecondElement.classList.add('hidden');
+            }
+        }
+    }
+}
