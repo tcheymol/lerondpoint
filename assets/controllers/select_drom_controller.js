4bbfe7596b50ac94cdedd17039377f2f4197a2ca
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2025-06-16 14:09:33 +0200

    :sparkles: [DROMS] Switch map show select

---
+++ assets/controllers/select_drom_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from '@hotwired/stimulus';
+
+/*
+* The following line makes this controller "lazy": it won't be downloaded until needed
+* See https://symfony.com/bundles/StimulusBundle/current/index.html#lazy-stimulus-controllers
+*/
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static values = {
+        url: String,
+    }
+
+    update(event) {
+        const selectedDrom = event.target.value;
+        if (!selectedDrom || !this.hasUrlValue) {
+            return;
+        }
+
+        window.location.replace(`${this.urlValue}/${selectedDrom}`);
+    }
+}
