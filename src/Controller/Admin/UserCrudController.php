4835c705a508043082b9ee0d8106a9e66b56c38f
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2025-07-16 14:32:47 +0200

    :sparkles: [User admin] Show username and dates

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -16,6 +20,12 @@ class UserCrudController extends AbstractCrudController
 {
     public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
     {
+    }
+
+    #[\Override]
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
     #[\Override]
@@ -29,10 +39,13 @@ class UserCrudController extends AbstractCrudController
     {
         yield IdField::new('id')->hideOnForm();
         yield EmailField::new('email');
+        yield TextField::new('username');
         yield TextField::new('plainPassword')->onlyOnForms();
         yield ChoiceField::new('roles', 'roles')->setChoices(User::ROLES)->allowMultipleChoices();
         yield BooleanField::new('disabled');
         yield BooleanField::new('hasAcceptedTerms')->onlyOnForms();
+        yield DateTimeField::new('createdAt')->hideOnForm();
+        yield DateTimeField::new('updatedAt')->onlyOnDetail();
     }
 
     #[\Override]
